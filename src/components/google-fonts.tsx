f0596d216e4744b67df618ae26f22a68fd44dbc6
github_student <jeremypoliquit.student@gmail.com>
2025-05-08 20:10:01 +0800

    Initial commit

---
+++ src/components/google-fonts.tsx
@@ -0,0 +1,37 @@
+import {
+  Montserrat,
+  Anton,
+  Rubik_Mono_One,
+  Fugaz_One,
+  Teko,
+  Lilita_One,
+  Sanchez,
+} from "next/font/google";
+
+export const montserrat = Montserrat({
+  weight: ["400", "700"], // Customize font weights
+});
+
+export const anton = Anton({
+  weight: ["400"],
+});
+
+export const rubik_mono_one = Rubik_Mono_One({
+  weight: ["400"],
+});
+
+export const fugaz_one = Fugaz_One({
+  weight: ["400"],
+});
+
+export const teko = Teko({
+  weight: ["400"],
+});
+
+export const lilita_one = Lilita_One({
+  weight: ["400"],
+});
+
+export const sanchez = Sanchez({
+  weight: ["400"],
+});
